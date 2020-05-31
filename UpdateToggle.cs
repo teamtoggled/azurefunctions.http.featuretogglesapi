b2eff0d0b444f97ca612186acdcb7e50e9b8401f
Ben Sloan <ben@toggled.dev>
2020-05-31 16:28:21 +0100

    Actually look up a feature toggle and try to change it

--- UpdateToggle.cs
+++ UpdateToggle.cs
@@ -10,26 +10,60 @@ using Newtonsoft.Json;
 
 namespace UpdateFeatureToggles
 {
+    public class LastUpdatedInfo 
+    {
+        public string On {get; set;}
+        public LastUpdatedBy By {get; set;}
+    }
+
+    public class LastUpdatedBy 
+    {
+        public Guid Id {get; set;}
+        public string Name {get; set;}
+    }
+    public class FeatureToggle 
+    {
+        public Guid Id {get; set;}
+        public string Type {get; set;}
+        public Guid ConfigurationId {get; set;}
+        public string Name {get; set;}
+        public bool State {get; set;}
+        public LastUpdatedInfo LastUpdated {get; set;}
+        public string SignalRVaultUrl {get; set;}
+    }
+
     public static class UpdateToggle
     {
         [FunctionName("UpdateToggle")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "configurations/{configurationId}/featuretoggles/{featureToggleId}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "toggled",
+                collectionName: "beta-featuretogles",
+                ConnectionStringSetting = "CosmosDbConnection",
+                Id = "{id}",
+                PartitionKey = "{configurationId}")] FeatureToggle featureToggleIn,
+            [CosmosDB(
+                databaseName: "toggled",
+                collectionName: "beta-featuretoggles",
+                ConnectionStringSetting = "CosmosDbConnection")]
+                IAsyncCollector<FeatureToggle> featureTogglesOut,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
+            if(featureToggleIn != null)
+            {
+                log.LogInformation($"Got a toggle named {featureToggleIn.Name}");
 
-            string name = req.Query["name"];
+                featureToggleIn.LastUpdated.On = DateTime.UtcNow.ToString("O");
+                await featureTogglesOut.AddAsync(featureToggleIn);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+                return new OkObjectResult("We've updated the LastUpdated.On of toggle.");
+            }
+            else
+            {
+                return new NotFoundObjectResult("Could not find a feature toggle for this configuration/id");
+            }
         }
     }
 }
